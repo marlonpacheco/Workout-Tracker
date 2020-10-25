f0507456da51bd1a918b66b7567d9625aeffa56e
Marlon Pacheco <pacheco.mjc@gmail.com>
2020-10-25 17:15:28 -0500

    setup server and model files

---
+++ models/workout.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+
+    day: Date,
+    exercises: [
+        {
+            type: {
+                type: String,
+                required: "Cardio or Resistance",
+                trim: true
+            },
+            name: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            duration: {
+                type: Number,
+                required: "Duration in minutes",
+                trim: true
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            }
+        }
+    ]
+
+})
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
